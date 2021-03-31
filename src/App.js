857a24e18e41b1733980d49448ac7d0e34b299cf
Doun Douns <sangodoun@protonmail.com>
2021-03-31 13:29:15 +0200

    premier test React

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import Navbar from "./components/Navbar";
+import QuoteCard from "./components/QuoteCard";
 
 function App() {
   return (
     <div className="App">
-    
+        
+        {/*Appel de la fonction Navbar*/}
+      <Navbar />
+      
+      <QuoteCard />
     </div>
   );
 }
